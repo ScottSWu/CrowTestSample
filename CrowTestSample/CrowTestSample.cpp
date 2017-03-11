c7f23dbef07895a9e2e29ac9c44ce720c0a9e297
lauralindy <laura.f.lin@gmail.com>
2017-03-11 00:33:28 -0500

    adding GUI

--- CrowTestSample/CrowTestSample.cpp
+++ CrowTestSample/CrowTestSample.cpp
@@ -8,6 +8,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <urlmon.h>
+#pragma comment( lib, "urlmon" )
+
 
 std::vector< char > fileContents;
 
@@ -18,7 +21,7 @@ struct Middleware
 	Middleware()
 	{
 		message = " ";
-	}
+	}	
 
 	void setMessage(std::string newMsg)
 	{
@@ -55,17 +58,54 @@ std::string readIn(std::string fileName) {
 	return std::string(fileContents.data(), fileContents.size());
 }
 
+std::wstring MimeTypeFromString(const std::wstring& str) {
+
+	LPWSTR pwzMimeOut = NULL;
+	HRESULT hr = FindMimeFromData(NULL, str.c_str(), NULL, 0,
+		NULL, FMFD_URLASFILENAME, &pwzMimeOut, 0x0);
+	if (SUCCEEDED(hr)) {
+		std::wstring strResult(pwzMimeOut);
+		// Despite the documentation stating to call operator delete, the
+		// returned string must be cleaned up using CoTaskMemFree
+		CoTaskMemFree(pwzMimeOut);
+		return strResult;
+	}
+
+	return L"";
+}
+
+// Convert an UTF8 string to a wide Unicode String
+std::wstring utf8_decode(const std::string &str)
+{
+	if (str.empty()) return std::wstring();
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+	std::wstring wstrTo(size_needed, 0);
+	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+	return wstrTo;
+}
+
+// Convert a wide Unicode string to an UTF8 string
+std::string utf8_encode(const std::wstring &wstr)
+{
+	if (wstr.empty()) return std::string();
+	int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+	std::string strTo(size_needed, 0);
+	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+	return strTo;
+}
+
 int main(){
 	
 	crow::App<Middleware> app;
 	app.get_middleware<Middleware>().setMessage("hello");
-	std::string fileName = "Text.txt";
-	std::string fileNameS = "/" + fileName;
-	std::cout << fileNameS;
 	
 	crow::mustache::set_base(".");
 	
 	CROW_ROUTE(app, "/<string>") ([](const crow::request& req, crow::response& res, std::string str) {
+		int index = str.rfind('.');
+		std::wstring place = utf8_decode(str.substr(index));
+		std::string str1(utf8_encode(MimeTypeFromString(place)));
+		res.add_header("Content-Type", str1);
 		res.write(readIn(str));
 		res.end();
 	});
